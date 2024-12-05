108431f4b746fc90c0fd9d9b8b19135aef876fcd
Sergio Zambrano <szambrano@agroifabril.com>
2024-12-05 08:31:23 -0500

    Modificacion puerto trabajo 3000

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const pool = require('./db');
 require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
